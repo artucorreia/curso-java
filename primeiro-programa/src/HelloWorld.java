f8bf524ce724e3ecd387a5d480088a230903056e
Arthur Correia <arthurcorria0109@gmail.com>
2024-02-23 10:37:02 -0300

    start

---
+++ primeiro-programa/src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+
+    public void displayMsg(String msg) {
+        System.out.println(msg);
+    }
+}
---
+++ primeiro-programa/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        HelloWorld display = new HelloWorld();
+        String msg = "Hello World!";
+        display.displayMsg(msg);
+    }
+}
