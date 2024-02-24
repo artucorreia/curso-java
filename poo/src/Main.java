49db2999fd2abf4f595127de3e183009939cc640
Arthur Correia <arthurcorria0109@gmail.com>
2024-02-24 00:24:46 -0300

    poo

---
+++ poo/src/Main.java
@@ -0,0 +1,8 @@
+import aplication.Triangle;
+
+public class Main {
+    public static void main(String[] args) {
+        Triangle firstTriangle = new Triangle();
+        Triangle seccondTriangle = new Triangle();
+    }
+}
---
+++ poo/src/aplication/Triangle.java
@@ -0,0 +1,11 @@
+package aplication;
+
+public class Triangle {
+    public double a;
+    public double b;
+    public double c;
+
+    public void area() {
+        return ;
+    }
+}
