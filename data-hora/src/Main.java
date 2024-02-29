46ef0ad836157c312d9480ac4c2be8ec20eeb531
Arthur Correia <arthurcorria0109@gmail.com>
2024-02-29 11:07:32 -0300

    data hora

---
+++ data-hora/src/Main.java
@@ -0,0 +1,54 @@
+import java.text.DateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Data Local sem horas
+        LocalDate d1 = LocalDate.now();
+        System.out.println(d1); // 2024-02-29
+
+        // Data Local com horas
+        LocalDateTime d2 = LocalDateTime.now();
+        System.out.println(d2); // 2024-02-29T10:20:05.917366300
+
+        // Data global zulu
+        Instant d3 = Instant.now();
+        System.out.println(d3); // 2024-02-29T13:20:05.918464700Z
+
+        // Data padrão ISO para LocalDate
+        LocalDate d4 = LocalDate.parse("2024-01-21");
+        System.out.println(d4); // 2024-01-21
+
+        // Data padrão ISO para LocalDateTime
+        LocalDateTime d5 = LocalDateTime.parse("2024-01-21T15:30:10");
+        System.out.println(d5); // 2024-01-21T15:30:10
+
+        // Data padrão ISO para Instant
+        Instant d6 = Instant.parse("2024-01-21T15:30:10Z");
+        System.out.println(d6); // 2024-01-21T15:30:10Z
+
+        // Data padrão ISO com fuso horário para Instant
+        Instant d7 = Instant.parse("2024-01-21T15:30:10-03:00");
+        System.out.println(d7); // 2024-01-21T18:30:10Z
+
+        // Data em outros formatos para LocalDate
+        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate d8 = LocalDate.parse("04/12/2021", format1);
+        System.out.println(d8); // 2021-12-04
+
+        // Data em outros formatos para LocalDateTime
+        LocalDateTime d9 = LocalDateTime.parse(
+                "04/12/2021 20:30",
+                DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")
+        );
+        System.out.println(d9); // 2021-12-04T20:30
+
+        // LocalDate inserindo um dado por vez
+        LocalDate d10 = LocalDate.of(2021, 12, 04);
+        System.out.println(d10); // 2021-12-04
+    }
+}
--- modulo-11/src/aplication/Main.java
+++ modulo-11/src/aplication/Main.java
@@ -12,6 +12,8 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner scan = new Scanner(System.in);
 
+        // Array list
+        /*
         ArrayList<Employee> employees = new ArrayList<>();
 
         System.out.print("How many employees will be registered? ");
@@ -52,6 +54,88 @@ public class Main {
         for (Employee employee : employees) {
             System.out.println(employee);
         }
+        */
+
+        // Matriz
+        /*
+        int n = scan.nextInt();
+        int[][] matriz = new int[n][n];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = scan.nextInt();
+            }
+        }
+        System.out.println("----------//---------");
+
+        int[] mainDiagonal = new int[n];
+        int negativeNumbers = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (i == j) {
+                    mainDiagonal[i] = matriz[i][j];
+                }
+
+                if (matriz[i][j] < 0) { negativeNumbers++; }
+
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("----------//---------");
+
+        System.out.println(
+                "Main diagonal: " +
+                mainDiagonal[0] +
+                " " +
+                mainDiagonal[1] +
+                " " +
+                mainDiagonal[2]
+        );
+        System.out.println("Negative numbers = " + negativeNumbers);
+        */
+
+        // Matriz 2
+        /*
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+
+        int[][] matriz = new int[m][n];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = scan.nextInt();
+            }
+        }
+
+        int number = scan.nextInt();
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if(matriz[i][j] == number) {
+                    System.out.printf("Position %d,%d:\n", i, j);
+                    if (j - 1 >= 0) {
+                        System.out.println("Left: " + matriz[i][j-1]);
+                    }
+
+                    if (j + 1 < matriz[i].length) {
+                        System.out.println("Right: " + matriz[i][j+1]);
+                    }
+
+                    if (i - 1 >= 0) {
+                        System.out.println("Up: " + matriz[i-1][j]);
+                    }
+
+                    if (i + 1 < matriz.length) {
+                        System.out.println("Down: " + matriz[i+1][j]);
+                    }
+                }
+            }
+            System.out.println();
+        }
+
+         */
 
         scan.close();
     }
