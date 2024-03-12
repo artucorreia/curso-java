7d3fd7a22c3ec58bf7b4be101910a9dbd3f70f73
Arthur Correia <arthurcorria0109@gmail.com>
2024-03-12 11:17:41 -0300

    Update Main.java

--- modulo-15/src/Main.java
+++ modulo-15/src/Main.java
@@ -1,5 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        File file = new File("C:\\Users\\Arthur\\Documents\\gym\\treino.txt");
+        Scanner scan = null;
+
+        try {
+            scan = new Scanner(file);
+            while (scan.hasNextLine()) {
+                System.out.println(scan.nextLine());
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Error opening file: " + e.getMessage());
+        }
+        finally {
+            if (scan != null) {
+                scan.close();
+            }
+            System.out.println("finally block executed");
+        }
+
     }
 }
